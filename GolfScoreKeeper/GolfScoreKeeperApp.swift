887d0c4e42cd3818adfd8fa8bb2dd6475aa9aaf7
BrandonTAnzalone <54599961+BrandonTAnzalone@users.noreply.github.com>
2022-03-23 08:59:01 -0500

    Initial Commit

---
+++ GolfScoreKeeper/GolfScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfScoreKeeperApp.swift
+//  GolfScoreKeeper
+//
+//  Created by Brandon Anzalone on 3/23/22.
+//
+
+import SwiftUI
+
+@main
+struct GolfScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
