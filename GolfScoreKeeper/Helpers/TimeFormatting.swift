4ac4c0bd02471b5a600b0e38d65ac86460edd2f0
BrandonTAnzalone <54599961+BrandonTAnzalone@users.noreply.github.com>
2022-05-16 13:55:34 -0500

    Tester

--- GolfScoreKeeper/Helpers/TimeFormatting.swift
+++ GolfScoreKeeper/Helpers/TimeFormatting.swift
@@ -10,7 +10,7 @@ import Foundation
 func calcTimeSince(date: Date) -> String {
     let minutes = Int(-date.timeIntervalSinceNow)/60
     let hours = minutes/60
-    let days = hours/24
+    //let days = hours/24
     
     if minutes < 120 {
         return "\(minutes) mintues ago"
